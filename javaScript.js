b1d4ad9deca2c82126caf4e5291d8908f6237c4c
Parker-Teske <pteske325@gmail.com>
2023-07-07 15:33:05 -0400

    tilelayer

--- javaScript.js
+++ javaScript.js
@@ -2,3 +2,7 @@ let myMap = L.map("map", {
   center: [41.682555, -72.652228],
   zoom: 4
 });
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(myMap);
