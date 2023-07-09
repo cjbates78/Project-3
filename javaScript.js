9a305e69dd88394ac7c866c7500096242e4fdf2e
cjbates78 <127440985+cjbates78@users.noreply.github.com>
2023-07-09 13:04:12 -0400

    Made to changes to html, js and css to start AOS

--- javaScript.js
+++ javaScript.js
@@ -1,8 +1,12 @@
-let myMap = L.map("map", {
-  center: [41.682555, -72.652228],
-  zoom: 4
-});
+AOS.init({
+  duration: 1200
+ });
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(myMap);
+//let myMap = L.map("map", {
+ // center: [41.682555, -72.652228],
+  //zoom: 4
+//});
+
+//L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+//}).addTo(myMap);
