1877ee657a7c3022bf5db7a72e570e88d58d4ec7
cjbates78 <127440985+cjbates78@users.noreply.github.com>
2023-07-15 20:34:50 -0400

    Updating files

--- javaScript.js
+++ javaScript.js
@@ -1,12 +1,3 @@
 AOS.init({
   duration: 1200
  });
-
-//let myMap = L.map("map", {
- // center: [41.682555, -72.652228],
-  //zoom: 4
-//});
-
-//L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-//}).addTo(myMap);
